848c41ac858232447510a9e37bc1d9befced9d78
Nagasawa <ns1997.dev@gmail.com>
2020-05-24 22:53:14 +0900

    Controllerに必要メソッド作成

--- src/main/java/core/api/IVController.java
+++ src/main/java/core/api/IVController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import core.api.entity.ImageRegisterForm;
@@ -27,8 +28,38 @@ public class IVController {
 	}
 
 	@PostMapping("/")
-	public String registerImage(@ModelAttribute ImageRegisterForm form) throws IOException {
+	public String uploadImage(@ModelAttribute ImageRegisterForm form) throws IOException {
 		ivService.registerImage(form);
 		return "redirect:/";
+	}
+
+	// 画像ファイルをダウンロード
+	@PostMapping("/download")
+	public void downloadImage() {
+
+	}
+
+	// 画像リストを並べ替え
+	@GetMapping("/sort")
+	public String sortImages() {
+		return null;
+	}
+
+	// タグ名で検索
+	@GetMapping("/search/{tag}")
+	public String searchByTagName() {
+		return null;
+	}
+
+	// 削除依頼を申請
+	@PatchMapping("/request/deletion")
+	public void requestDeletion() {
+
+	}
+
+	// いいねを増やす
+	@PatchMapping("/count/good")
+	public void countGood() {
+
 	}
 }
